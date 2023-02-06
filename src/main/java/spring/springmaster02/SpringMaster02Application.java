74101d114d75a7f9cf47c7a00a5c9b838d51b169
prolkh <prolkh@gmail.com>
2023-02-06 23:45:29 +0900

    스프링 핵심 원리 - 기본편 프로젝트 생성

---
+++ src/main/java/spring/springmaster02/SpringMaster02Application.java
@@ -0,0 +1,13 @@
+package spring.springmaster02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaster02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMaster02Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring/springmaster02/SpringMaster02ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springmaster02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMaster02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
