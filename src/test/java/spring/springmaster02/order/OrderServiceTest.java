e7f49edf5615e30082460d9df34a802b4451ff28
prolkh <prolkh@gmail.com>
2023-02-09 23:58:14 +0900

    주문과 할인 도메인 테스트 코드를 작성하다.

---
+++ src/test/java/spring/springmaster02/order/OrderServiceTest.java
@@ -0,0 +1,31 @@
+package spring.springmaster02.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import spring.springmaster02.member.Grade;
+import spring.springmaster02.member.Member;
+import spring.springmaster02.member.MemberService;
+import spring.springmaster02.member.MemberServiceImpl;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+
+    @Test
+    @DisplayName("등급 할인이 정상적으로 적용되다.")
+    void discountPrice() {
+        // Given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // When
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // Then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
