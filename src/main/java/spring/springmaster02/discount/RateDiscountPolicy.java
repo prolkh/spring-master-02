b136585b09961c625dfde417e243966f14c2d748
prolkh <prolkh@gmail.com>
2023-02-10 23:20:58 +0900

    새로운 할인 정책을 개발하고 테스트하다.

---
+++ src/main/java/spring/springmaster02/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package spring.springmaster02.discount;
+
+import spring.springmaster02.member.Grade;
+import spring.springmaster02.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
