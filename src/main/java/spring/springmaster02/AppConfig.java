ff7670b51fd356aa87f0c3e7f0b3c42dc39fcdc7
prolkh <prolkh@gmail.com>
2023-02-11 22:07:16 +0900

    역할과 구현 클래스가 한 눈에 들어오도록 리팩토링하다.

--- src/main/java/spring/springmaster02/AppConfig.java
+++ src/main/java/spring/springmaster02/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.springmaster02;
 
+import spring.springmaster02.discount.DiscountPolicy;
 import spring.springmaster02.discount.FixDiscountPolicy;
+import spring.springmaster02.member.MemberRepository;
 import spring.springmaster02.member.MemberService;
 import spring.springmaster02.member.MemberServiceImpl;
 import spring.springmaster02.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import spring.springmaster02.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
