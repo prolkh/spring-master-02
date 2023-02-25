44bdc59ac131d1ad460fbda50e564efbfa89f71c
prolkh <prolkh@gmail.com>
2023-02-25 23:49:10 +0900

    롬복 라이브러리를 추가하고 @RequiredArgsConstructor 를 적용해서 @Autowired 생성자를 제거하다.

--- src/main/java/spring/springmaster02/order/OrderServiceImpl.java
+++ src/main/java/spring/springmaster02/order/OrderServiceImpl.java
@@ -1,22 +1,17 @@
 package spring.springmaster02.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import spring.springmaster02.discount.DiscountPolicy;
 import spring.springmaster02.member.Member;
 import spring.springmaster02.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
