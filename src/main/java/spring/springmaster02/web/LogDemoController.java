a60754dd3235ea7eca4adea0e90bac06af1d6e16
prolkh <prolkh@gmail.com>
2023-03-06 23:46:55 +0900

    request 스코프에 프록시를 설정해보다.

--- src/main/java/spring/springmaster02/web/LogDemoController.java
+++ src/main/java/spring/springmaster02/web/LogDemoController.java
@@ -2,7 +2,6 @@ package spring.springmaster02.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +12,14 @@ import spring.springmaster02.common.MyLogger;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
